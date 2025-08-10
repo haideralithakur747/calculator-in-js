2afade8f4aa713270a8b1e018d9f35b92c1b6689
Haider ALi <haideralithakur747@gmail.com>
2025-08-11 01:16:56 +0500

    60 percent compeletion

--- functionality.js
+++ functionality.js
@@ -1,5 +1,43 @@
 let firstnumber= null
 let secondnumber= null
 let operator= null
+let displayvalue= "";
 
-// switch
+function buttonClick(value) {
+    switch(value) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            displayvalue += value;
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            firstnumber = displayvalue;
+            operator = value;
+            displayvalue = "";
+            break;
+        case '=':
+            secondnumber = displayvalue;
+            calculate();
+            break;
+        case 'C':
+            clear();
+            break;
+    }
+    updateValue();
+}
+   function updateValue() {
+    const display = document.querySelector('.display');
+    display.textContent = displayvalue;
+
+
+}
